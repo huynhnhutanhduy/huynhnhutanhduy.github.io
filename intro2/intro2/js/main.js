d3b7e91876451878a436a66bb42245d8c15aaabe
Huynh Nhut Anh Duy <duy.hnad@gmail.com>
2021-04-16 21:45:28 +0700

    update

--- intro2/intro2/js/main.js
+++
@@ -1,1 +0,0 @@
-var options={strings:["Hi! I'm Huynh Nhut Anh Duy","Welcome to my Website","Have a nice day","Click play button!"],typeSpeed:30,backSpeed:30,loop:true};var typed=new Typed('.typing',options);
