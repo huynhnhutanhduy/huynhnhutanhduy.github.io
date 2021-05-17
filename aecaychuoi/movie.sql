5e62f76044b88d642021837428ec72fdf30b71f7
Huynh Nhut Anh Duy <duy.hnad@gmail.com>
2021-05-17 15:11:31 +0700

    delete aecaychuoi

--- aecaychuoi/movie.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `aecaychuoi`.`admin` ( 
-    `username` VARCHAR(50) NOT NULL , 
-    `password` VARCHAR(32) NOT NULL , 
-    PRIMARY KEY (`username`)
-) ENGINE = InnoDB;
-
-INSERT INTO `admin` (`username`, `password`) VALUES ('aecaychuoi', 'aecaychuoi');
